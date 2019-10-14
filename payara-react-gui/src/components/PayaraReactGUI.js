47a2876458038d24b457845b327c0c8e7fa8d9b3
Anders Thrane Michelsen <atm@dbc.dk>
2019-10-14 13:11:42 +0200

    Add basic maven structure
    Auditors: jbn, jbr

---
+++ payara-react-gui/src/components/PayaraReactGUI.js
@@ -0,0 +1,24 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+ * See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+ */
+
+import React from "react";
+
+class PayaraReactGUI extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p>Payara React Example</p>
+            </div>
+        )
+    }
+
+}
+
+export default PayaraReactGUI;
